50d83414f9aba07d67a78c3ab986f13234d263e4
JiYong <rajy12@hanmail.net>
2019-11-19 09:47:59 +0900

    first commit

---
+++ Day10/forecast/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <header>
+                <h1>일기 예보</h1>
+            </header>
+        </>
+    )
+};
+
+export default App;
